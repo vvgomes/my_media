e7453903974178663e8f8dae49d471dcffacebff
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2015-02-28 18:05:10 -0300

    adds validation on media kinds

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,5 +1,8 @@
 class MediaItem < ActiveRecord::Base
+  enum :kind => [:video, :image, :website]
+
   validates :kind, :presence => true
   validates :url, :presence => true,
     :format => { :with => URI::regexp(%w(http https)), :message => 'Invalid URL' }
+
 end
--- spec/models/media_item_spec.rb
+++ spec/models/media_item_spec.rb
@@ -3,8 +3,16 @@ require 'rails_helper'
 describe MediaItem, :type => :model do
   subject { build(:media_item) }
 
-  it { is_expected.to validate_presence_of :kind }
   it { is_expected.to validate_presence_of :url }
   it { is_expected.to allow_value('http://google.com').for(:url) }
   it { is_expected.not_to allow_value('not-an-url').for(:url) }
+
+  it { is_expected.to validate_presence_of :kind }
+  it { is_expected.to allow_value('video').for(:kind) }
+  it { is_expected.to allow_value('image').for(:kind) }
+  it { is_expected.to allow_value('website').for(:kind) }
+
+  it 'does not allow invalid value for kind' do
+    expect { subject.kind = 'invalid' }.to raise_error
+  end
 end
